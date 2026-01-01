5a60e97f914281030a20525448bdcbebc75f6c15
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-01-01 14:54:36 -0500

    Added createGrid method + unit tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,26 @@
 // Gameboard class
 
+import { Square } from "./square.js";
+
 export class Gameboard {
   constructor() {
-    this.grid; // Add createGrid method call
+    this.grid = this.createGrid();
     this.shipsSunk = 0;
     this.shipsPlaced = 0;
     this.allShipsSunk = false;
     this.allShipsPlaced = false;
+  }
+
+  createGrid() {
+    const grid = [];
+
+    for (let r = 0; r < 10; r++) {
+      grid.push([]);
+      for (let c = 0; c < 10; c++) {
+        grid[r].push(new Square());
+      }
+    }
+
+    return grid;
   }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -4,6 +4,22 @@ import { Gameboard } from "./gameboard.js";
 
 describe("Gameboard constructor initalization", () => {
   const gameboard = new Gameboard();
+
+  test("Grid has initial correct square at coordinate (0,0)", () => {
+    expect(gameboard.grid[0][0].ship).toBe(null);
+    expect(gameboard.grid[0][0].beenAttacked).toBe(false);
+  });
+
+  test("Grid has initial correct sqaure at coordinate (5,5)", () => {
+    expect(gameboard.grid[5][5].ship).toBe(null);
+    expect(gameboard.grid[5][5].beenAttacked).toBe(false);
+  });
+
+  test("Grid has initial correct sqaure at coordinate (9,9)", () => {
+    expect(gameboard.grid[9][9].ship).toBe(null);
+    expect(gameboard.grid[9][9].beenAttacked).toBe(false);
+  });
+
   test("shipsSunk is initially 0", () => {
     expect(gameboard.shipsSunk).toBe(0);
   });
