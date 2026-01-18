879309194bdb19aff5f13a741848de1fbdd483ff
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-01-18 17:47:26 -0500

    tested removeBoardsFromUI method

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Battleship JS index
 
 import "./style.css";
-import { addBoardToUI } from "./gameUI.js";
+import { addBoardToUI, removeBoardsfromUI } from "./gameUI.js";
 import { Player } from "./player.js";
 
 const computer = new Player("Phoney baloney", "computer");
