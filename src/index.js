c8f59717652505cc6bbce1e75545de886ea51968
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-01-18 12:03:26 -0500

    Added test addBoardToUI call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // Battleship JS index
 
 import "./style.css";
+import { addBoardToUI } from "./gameUI.js";
+import { Player } from "./player.js";
+
+const computer = new Player("Phoney baloney", "computer");
+addBoardToUI(computer);
