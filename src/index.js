36136ca6902a7ebf684b23c6716f5f8851f85071
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-01-21 19:53:35 -0500

    tested placeShipOnSquareInUI

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { Player } from "./player.js";
 
 const human = new Player("Phoney baloney", "human");
 addBoardToUI(human);
-
-// TEST placeShipOnSquareInUI
-
-
+selectShipInUI("destroyer", human);
+placeShipOnSquareInUI(1, 1, human);
+placeShipOnSquareInUI(2, 1, human);
+selectShipInUI("cruiser", human);
+placeShipOnSquareInUI(5, 5, human);
+placeShipOnSquareInUI(5, 6, human);
+placeShipOnSquareInUI(5, 7, human);
