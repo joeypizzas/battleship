3be25beb7ce58a468e58695f0ca8776cab6dba92
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-02-22 14:34:11 -0500

    Wrote placeComputerShips method

---
+++ src/computer.js
@@ -0,0 +1,17 @@
+// Computer module
+
+const targetQueue = [];
+
+export function placeComputerShips(computer) {
+  const shipsForPlacement = [...computer.ships];
+
+  while (!computer.gameboard.allShipsPlaced) {
+    computer.selectedShip = shipsForPlacement.shift();
+    while (!computer.selectedShip.fullyPlaced) {
+      computer.selectedShip.addSquareToShipPlacement(Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), computer.gameboard);
+      computer.gameboard.placeShip(computer.selectedShip);
+    }
+  }
+
+  computer.removeSelectedShip();
+}
