902224f242fc4e7219e92bd30a66f82e2aad79f6
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-02-26 07:29:14 -0500

    Added logging to debug

--- src/computer.js
+++ src/computer.js
@@ -55,6 +55,7 @@ function delay(ms) {
 export async function computerAttack(human) {
   while (getCurrentPlayerTurn() === "computer") {
     const attackCoordinates = determineComputerAttack(human);
+    console.log(attackCoordinates);
     await delay(3000);
     attackSquareInUI(attackCoordinates[0], attackCoordinates[1], human, human);
     if (human.gameboard.grid[attackCoordinates[0]][attackCoordinates[1]].ship) {
